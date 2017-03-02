04239873316aefd391916af39a145a3b1c2ae253
programmerdave <davetheprogrammerdude@gmail.com>
2017-03-02 09:19:25 -0600

    made the boxes have random values

---
+++ tower-project/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    //gen values for the boxes
+    var b1 = Math.floor(Math.random() * 1000) + 1;
+    var b2 = Math.floor(Math.random() * 1000) + 1;
+    var b3 = Math.floor(Math.random() * 1000) + 1;
+    var b4 = Math.floor(Math.random() * 1000) + 1;
+    var b5 = Math.floor(Math.random() * 1000) + 1;
+    var b6 = Math.floor(Math.random() * 1000) + 1;
+    var b7 = Math.floor(Math.random() * 1000) + 1;
+    document.getElementById("BoxOne").innerHTML = b1;
+});
